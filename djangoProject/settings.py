d19f2afc41c45a803d158225a0a41f8e011d6bba
Muhammad <moli1369@gmail.com>
2024-05-01 13:41:39 +0330

    frist comit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my app
+    'account.apps.AccountConfig',
+    'product.apps.ProductConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -104,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
